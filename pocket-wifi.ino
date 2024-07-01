a61b4f2d8526390f414f0d4f1e18a1dcaa7ab75d
Levi Villarreal <villarreallevi@gmail.com>
2024-07-01 13:57:12 -0700

    Make website way smaller and faster

--- pocket-wifi.ino
+++ pocket-wifi.ino
@@ -15,7 +15,7 @@ const byte DNS_PORT = 53;
 ESP8266WebServer server(80);
 
 #ifndef STASSID
-#define STASSID "✈️ Airplane Reading WiFi ✈️"
+#define STASSID " 🚅 Amtrak Reading WiFi"
 #endif
 
 IPAddress apIP(192, 168, 4, 1);
